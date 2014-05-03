e9cc440e6355fadf35bb703158553167b6f3d7a6
ran488 <ran488@gmail.com>
2014-05-02 22:42:27 -0400

    updating versions of spring in build dependencies, moving some stuff around, removing unused stuff, etc.

--- src/main/java/com/samuraiswap/web/SwapMatcherController.java
+++ src/main/java/com/samuraiswap/web/SwapMatcherController.java
@@ -1,10 +1,27 @@
 package com.samuraiswap.web;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 @RequestMapping(value="api/findMatches")
 public class SwapMatcherController {
 
-}
+		@RequestMapping(value = "/hello", method = RequestMethod.GET)
+		public String hello(ModelMap model) {
+			model.addAttribute("msg", "JCG Hello World!");
+			return "helloWorld";
+		}
+		
+		@RequestMapping(value = "/displayMessage/{msg}", method = RequestMethod.GET)
+		public String displayMessage(@PathVariable String msg, ModelMap model) {
+			model.addAttribute("msg", msg);
+			return "helloWorld";
+		}
+		
+	}
